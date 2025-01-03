3778564d192e57a5ec652ca1a67b358569a675e1
张振中 <763557238@qq.com>
2025-01-03 16:14:58 +0800

    day20250103课件源码api更新

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-
-// 导航路由的遍历渲染，无用，可以忽略
-// import { ref } from 'vue';
-// import { routerList } from '@/router/index'
-// const routeList = ref<any>(routerList)
 </script>
 <template>
-  <!-- 导航路由的遍历渲染，无用，可以忽略 -->
-  <!-- <nav> -->
-  <!-- <RouterLink v-for="item in routeList" :key="item.name" :to="{ path: item.path }">{{item.title || item.name}}</RouterLink> -->
-  <!-- </nav> -->
-  <!-- <hr /> -->
   <RouterView />
 </template>
 
